a4cff1b2c9afc1895255820fba9671eb0603f038
ming <daytime25@gmail.com>
2015-10-04 22:56:07 +0300

    rtr

---
+++ fristtest/test_var.py
@@ -0,0 +1,30 @@
+﻿from nose import with_setup
+#from tee import shuzi
+#from tee import below_f
+from calc1 import Interpreter
+import nose
+
+
+def my_setup():
+  print "为测试执行配置代码"
+
+def my_teardown():
+  print "为测试执行清理代码"
+
+@with_setup(my_setup,my_teardown)
+def test_token():    
+    token=token('INTEGER, 3')
+    
+    
+    
+def  test_interpreter():
+    interpreter = Interpreter('3*55*456-55')
+    interpreter.get_next_token()
+    
+
+    
+    
+
+
+
+result=nose.run()
